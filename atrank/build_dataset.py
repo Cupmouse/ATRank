e8901cfa4f6a5d7720626f9a39aa808fa21e82ba
Nozomu <simaoka98@gmail.com>
2020-12-21 08:23:47 +0900

    コメント

--- atrank/build_dataset.py
+++ atrank/build_dataset.py
@@ -7,10 +7,13 @@ random.seed(1234)
 
 #user_count, item_count, cate_count, example_countはそれぞれユーザ、アイテム、カテゴリ、レビュー履歴の数
 with open('../raw_data/remap.pkl', 'rb') as f:
+  # asinが整数になっているレビューのデータ
   reviews_df = pickle.load(f)
+  # asinでソートされた商品に対するカテゴリ（商品のカテゴリ）の羅列
   cate_list = pickle.load(f)
   user_count, item_count, cate_count, example_count = pickle.load(f)
 
+# 時間をカテゴリカルな値にするためのテーブル
 # [1, 2) = 0, [2, 4) = 1, [4, 8) = 2, [8, 16) = 3...  need len(gap) hot
 gap = np.array([2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096])
 # gap = [2, 7, 15, 30, 60,]
@@ -18,47 +21,60 @@ gap = np.array([2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096])
 # gap = np.array(gap)
 print(gap.shape[0])
 
-#タイムスタンプを連続値→カテゴリ特徴に変換
-#gapで時間を区切る領域を指定し、領域ごとにカテゴリの数値を割り当てている。
 def proc_time_emb(hist_t, cur_t):
-  hist_t = [cur_t - i + 1 for i in hist_t]
-  hist_t = [np.sum(i >= gap) for i in hist_t]
+  """
+  絶対タイムスタンプを連続値→相対タイムスタンプのカテゴリ値に変換
+  hist_t：行動した絶対時間（日）のリスト
+  cur_t：最後の行動の絶対時間（日）
+  """
+  # 最後の行動との時間差をとってカテゴリ特徴へ
+  hist_t = [cur_t - abs_time + 1 for abs_time in hist_t]
+  # sum([2, 4, 8...] <= rel_time)＝カテゴリ値
+  hist_t = [np.sum(rel_time >= gap) for rel_time in hist_t]
   return hist_t
 
 train_set = []
 test_set = []
-#histは各reviewerIDについてのレビューデータ(reviewerID以外のカラム全て)
+# histは各reviewerIDについてのレビューデータ(reviewerID以外のカラム全て)
 for reviewerID, hist in reviews_df.groupby('reviewerID'):
+  # 正例のラベル
   pos_list = hist['asin'].tolist()
+  # 時間のリスト
   tim_list = hist['unixReviewTime'].tolist()
+  # 時間を1日単位に変換
   tim_list = [i // 3600 // 24 for i in tim_list]
-  #ユーザがレビューしたアイテムにランダムな数値を割り当てる
   def gen_neg():
+    """一つの行動に対する負例の生成"""
+    # 他の正例とかぶらないように繰り返しランダムでカテゴリ整数を生成
     neg = pos_list[0]
     while neg in pos_list:
       neg = random.randint(0, item_count-1)
     return neg
+  # 正例と同じ数だけ負例を生成
   neg_list = [gen_neg() for i in range(len(pos_list))]
 
-  #レビュー履歴とそれ以前の全てのレビュー履歴との時間差をとってカテゴリ特徴に直す
-  #一番最後のレビュー履歴はテストデータセットに、他は訓練データセットに入れている
+  # 訓練データを増やすために、元データをスライスする
   for i in range(1, len(pos_list)):
     hist_i = pos_list[:i]
     hist_t = proc_time_emb(tim_list[:i], tim_list[i])
+    # 一番最後の履歴はテストに、他は訓練に入れる
     if i != len(pos_list) - 1:
+      # (ユーザー, 履歴, 履歴時間, ラベル, 正例なら1でないなら0)
       train_set.append((reviewerID, hist_i, hist_t, pos_list[i], 1))
       train_set.append((reviewerID, hist_i, hist_t, neg_list[i], 0))
     else:
+      # (ユーザー, 履歴, 履歴時間, (正例,負例))
       label = (pos_list[i], neg_list[i])
       test_set.append((reviewerID, hist_i, hist_t, label))
 
+# シャフル
 random.shuffle(train_set)
 random.shuffle(test_set)
 
 assert len(test_set) == user_count
 # assert(len(test_set) + len(train_set) // 2 == reviews_df.shape[0])
 
-#train_set・test_setは一つ一つのレビュー履歴についてユーザID、レビューしたアイテムID、これ以前にレビューしたアイテム、過去の履歴との時間差を含む
+# train_set・test_setは一つ一つのレビュー履歴についてユーザID、レビューしたアイテムID、これ以前にレビューしたアイテム、過去の履歴との時間差を含む
 with open('dataset.pkl', 'wb') as f:
   pickle.dump(train_set, f, pickle.HIGHEST_PROTOCOL)
   pickle.dump(test_set, f, pickle.HIGHEST_PROTOCOL)
--- atrank/input.py
+++ atrank/input.py
@@ -1,45 +1,69 @@
 import numpy as np
 
 class DataInput:
+  """
+  整形済み学習・テストデータから指定されたバッチを生成
+  Iterable & Iterator
+  """
   def __init__(self, data, batch_size):
-    
-    #データサイズ、batch_sizeからepoch_sizeを決定
+    """
+    data：学習またはテストデータ
+    batch_size：バッチサイズ
+    """
     self.batch_size = batch_size
     self.data = data
+    # エポック数の計算
     self.epoch_size = len(self.data) // self.batch_size
     if self.epoch_size * self.batch_size < len(self.data):
       self.epoch_size += 1
+    # 現在のバッチインデックス
     self.i = 0
 
   def __iter__(self):
+    """イテレーターとして自分自身を返す"""
     return self
 
-  #次の状態(i)とデータを出力
   def __next__(self):
-
+    """
+    イテレーターが呼ばれた
+    次のバッチを返す
+    """
     if self.i == self.epoch_size:
+      # データが尽きた
       raise StopIteration
 
-    #次入力するデータを現在の状態iに従って持ってくる
-    ts = self.data[self.i * self.batch_size : min((self.i+1) * self.batch_size,
-                                                  len(self.data))]
-    #次に学習するデータの場所
+    # このバッチに関係のあるデータをスライス
+    # 「tsには行動の一覧」がリストになっている、一つの問題と同じ
+    ts = self.data[self.i * self.batch_size : min((self.i+1) * self.batch_size, len(self.data))]
+    # 次のバッチインデックスへ
     self.i += 1
 
-    #今回入力するデータを整形
+    # 行列へ入れ込む
+    # u:ユーザーID
+    # i:ラベル
+    # y:正例なら1、負例なら0
+    # sl:履歴の長さ
     u, i, y, sl = [], [], [], []
     for t in ts:
       u.append(t[0])
       i.append(t[3])
       y.append(t[4])
       sl.append(len(t[1]))
+    
+    # 最大の履歴の長さに合わせて行列を初期化する
     max_sl = max(sl)
 
+    # hist_i:行動履歴
     hist_i = np.zeros([len(ts), max_sl], np.int64)
+    # hist_t:行動の時間（hist_i[i]の時間）
+    # 行動時間は最後の行動からの相対時間になっている
     hist_t = np.zeros([len(ts), max_sl], np.float32)
 
+    # hist_iとhist_tに内容を書き込み
     k = 0
     for t in ts:
+      # t[1]は行動の履歴
+      # t[2]は行動の時間の履歴
       for l in range(len(t[1])):
         hist_i[k][l] = t[1][l]
         hist_t[k][l] = t[2][l]
@@ -48,9 +72,10 @@ class DataInput:
     return self.i, (u, i, y, hist_i, hist_t, sl)
 
 class DataInputTest:
+  """DataInputのテストデータバージョン"""
   def __init__(self, data, batch_size):
 
-    #epoch_sizeを決定
+    # epoch_sizeを決定
     self.batch_size = batch_size
     self.data = data
     self.epoch_size = len(self.data) // self.batch_size
@@ -61,20 +86,23 @@ class DataInputTest:
   def __iter__(self):
     return self
 
-  #次の状態(i)とデータを出力
+  # 次の状態(i)とデータを出力
   def __next__(self):
 
     if self.i == self.epoch_size:
       raise StopIteration
 
-    #今回入力するデータを現在の状態iに従って持ってくる
-    ts = self.data[self.i * self.batch_size : min((self.i+1) * self.batch_size,
-                                                  len(self.data))]
-    #次の状態
+    # 今回入力するデータを現在の状態iに従って持ってくる
+    ts = self.data[self.i * self.batch_size : min((self.i+1) * self.batch_size, len(self.data))]
+    # 次の状態
     self.i += 1
 
-    #今回入力するデータを整形
-    #DataInputと違うのはこの部分
+    # 今回入力するデータを整形
+    # DataInputと違うのはこの部分
+    # u:ユーザーID
+    # i:正例のラベル
+    # j:負例のラベル
+    # sl:履歴の長さ
     u, i, j, sl = [], [], [], []
     for t in ts:
       u.append(t[0])
