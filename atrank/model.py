62daf63aeeca5db3743ee42ebca9be4b5d0373eb
Nozomu <simaoka98@gmail.com>
2020-12-22 15:04:02 +0900

    コメント

--- atrank/model.py
+++ atrank/model.py
@@ -3,9 +3,11 @@ import json
 import numpy as np
 import tensorflow.compat.v1 as tf
 
-# モデルのクラスを定義
+
 
 class Model(object):
+  """モデルのクラスを定義"""
+
   def __init__(self, config, cate_list):
     """config：設定、cate_list：商品のカテゴリ（ASINのID順）"""
     self.config = config
@@ -53,55 +55,68 @@ class Model(object):
 
   def build_model(self, cate_list):
     """モデルの構築"""
+
     # 変数の定義
+    # 商品の埋め込み表現が保存される行列 [|I|, di]
     item_emb_w = tf.get_variable(
         "item_emb_w",
         [self.config['item_count'], self.config['itemid_embedding_size']])
+    # 類似度のバイアスのベクトル [|I|]
     item_b = tf.get_variable(
         "item_b",
         [self.config['item_count'],],
         initializer=tf.constant_initializer(0.0))
+    # カテゴリの埋め込み表現が保存される行列 [|A|, da]
     cate_emb_w = tf.get_variable(
         "cate_emb_w",
         [self.config['cate_count'], self.config['cateid_embedding_size']])
+    # 各商品のIDとカテゴリIDのマップ（リスト） [|I|]
     cate_list = tf.convert_to_tensor(cate_list, dtype=tf.int64)
 
     # アイテム埋め込みとカテゴリ埋め込みと時間の埋め込みを結合、それをDenseで写像する
     # 論文：p3左のu_ij=h_emb
+    # 予測すべきアイテムの埋め込み表現 [B, di+da]
     i_emb = tf.concat([
         tf.nn.embedding_lookup(item_emb_w, self.i),
         tf.nn.embedding_lookup(cate_emb_w, tf.gather(cate_list, self.i)),
         ], 1)
+    # 予測すべきアイテムの重み [B]
     i_b = tf.gather(item_b, self.i)
 
+    # 入力する各履歴の埋め込み表現 [B, T, di+da]
     h_emb = tf.concat([
         tf.nn.embedding_lookup(item_emb_w, self.hist_i),
         tf.nn.embedding_lookup(cate_emb_w, tf.gather(cate_list, self.hist_i)),
         ], 2)
 
     if self.config['concat_time_emb'] == True:
+      # 時間の埋め込み表現を結合 [B, T, di+da+dt]
       t_emb = tf.one_hot(self.hist_t, 12, dtype=tf.float32)
       h_emb = tf.concat([h_emb, t_emb], -1)
       h_emb = tf.layers.dense(h_emb, self.config['hidden_units'])
     else:
+      # 時間の埋め込み表現をPE [B, T, di+da]
       t_emb = tf.layers.dense(tf.expand_dims(self.hist_t, -1),
                               self.config['hidden_units'],
                               activation=tf.nn.tanh)
       h_emb += t_emb
 
-    # !一つしか潜在空間がない
+    # アテンション機構を重ねる数
     num_blocks = self.config['num_blocks']
     num_heads = self.config['num_heads']
     dropout_rate = self.config['dropout']
+    # QKVをDenseで写像した後のサイズ C = di+da+dt or di+da
     num_units = h_emb.get_shape().as_list()[-1]
 
-    # セルフアテンションを適用
+    # トランスフォーマー
     # 論文：p4左数式(3)
+    # u_emb [B, C]
     u_emb, self.att, self.stt = attention_net(
         # uij
         h_emb,
         # ユーザーの履歴の長さ
         self.sl,
+        # デコーダーへの入力
         i_emb,
         num_units,
         num_heads,
@@ -111,7 +126,7 @@ class Model(object):
         False)
 
     # 予測
-    # 論文：p4右数式(7)&(8) f(h_t, et_u)
+    # 論文：p4右数式(7)&(8) f(h_t, et_u) reduce_sum([B, C]) [B]
     self.logits = i_b + tf.reduce_sum(tf.multiply(u_emb, i_emb), 1)
 
     # ============== Eval ===============
@@ -124,13 +139,14 @@ class Model(object):
     self.global_epoch_step_op = \
         tf.assign(self.global_epoch_step, self.global_epoch_step+1)
 
-    # ロスの定義
     # Loss
+    # L2正規化
     l2_norm = tf.add_n([
         tf.nn.l2_loss(u_emb),
         tf.nn.l2_loss(i_emb),
         ])
 
+    # ロス定義、ペアワイズ、シグモイド相互情報量
     self.loss = tf.reduce_mean(
         tf.nn.sigmoid_cross_entropy_with_logits(
             logits=self.logits,
@@ -265,15 +281,18 @@ def attention_net(enc, sl, dec, num_units, num_heads, num_blocks, dropout_rate, 
   """
   トランスフォーマー
   論文：p4 Self-Attention Layer
-  enc：ユーザー埋め込み表現
-  sl：履歴の長さ
+  enc：履歴のバッチ [B, T, di+da+dt or di+da]
+  sl：各履歴の長さ [B, T]
+  dec：デコーダーへの入力 [B, di+da]
   """
   with tf.variable_scope("all", reuse=reuse):
     with tf.variable_scope("user_hist_group"):
       # エンコーダー
       for i in range(num_blocks):
         with tf.variable_scope("num_blocks_{}".format(i)):
+          # セルフマルチヘッドアテンション
           ### Multihead Attention
+          # enc [B, Tq, C]
           enc, stt_vec = multihead_attention(queries=enc,
               queries_length=sl,
               keys=enc,
@@ -286,16 +305,19 @@ def attention_net(enc, sl, dec, num_units, num_heads, num_blocks, dropout_rate, 
               )
 
           ### Feed Forward
+          # enc [B, Tq, C]
           enc = feedforward(enc,
               num_units=[num_units // 4, num_units],
               scope="feed_forward", reuse=reuse)
-
+    # dec [B, 1, di+da]
     dec = tf.expand_dims(dec, 1)
     with tf.variable_scope("item_feature_group"):
       # デコーダー
       for i in range(num_blocks):
         with tf.variable_scope("num_blocks_{}".format(i)):
           ## Multihead Attention ( vanilla attention)
+          # decを使ってencにアテンション
+          # dec [B, 1, C]
           dec, att_vec = multihead_attention(queries=dec,
               queries_length=tf.ones_like(dec[:, 0, 0], dtype=tf.int32),
               keys=enc,
@@ -306,11 +328,13 @@ def attention_net(enc, sl, dec, num_units, num_heads, num_blocks, dropout_rate, 
               is_training=is_training,
               scope="vanilla_attention")
 
+          # dec [B, 1, C]
           ## Feed Forward
           dec = feedforward(dec,
               num_units=[num_units // 4, num_units],
               scope="feed_forward", reuse=reuse)
 
+    # [B, C]
     dec = tf.reshape(dec, [-1, num_units])
     return dec, att_vec, stt_vec
 
@@ -343,46 +367,59 @@ def multihead_attention(queries,
   Returns
     A 3d tensor with shape of (N, T_q, C)
   '''
-  """
-  マルチヘットアテンション
-  """
   with tf.variable_scope(scope, reuse=reuse):
     # Set the fall back option for num_units
     if num_units is None:
       num_units = queries.get_shape().as_list[-1]
 
+    # queries ----> dense(relu) ----> Q
+    #
+    # keys    --+-> dense(relu) ----> K
+    #           |
+    #           +-> dense(relu) ----> V
     # Linear projections, C = # dim or column, T_x = # vectors or actions
     Q = tf.layers.dense(queries, num_units, activation=tf.nn.relu)  # (N, T_q, C)
     K = tf.layers.dense(keys, num_units, activation=tf.nn.relu)  # (N, T_k, C)
     V = tf.layers.dense(keys, num_units, activation=tf.nn.relu)  # (N, T_k, C)
 
+    # ヘッドで分割
     # Split and concat
     Q_ = tf.concat(tf.split(Q, num_heads, axis=2), axis=0)  # (h*N, T_q, C/h)
     K_ = tf.concat(tf.split(K, num_heads, axis=2), axis=0)  # (h*N, T_k, C/h)
     V_ = tf.concat(tf.split(V, num_heads, axis=2), axis=0)  # (h*N, T_k, C/h)
 
+    # アテンションスコアの算出（QK^T）
     # Multiplication
     # query-key score matrix
     # each big score matrix is then split into h score matrix with same size
     # w.r.t. different part of the feature
     outputs = tf.matmul(Q_, tf.transpose(K_, [0, 2, 1]))  # (h*N, T_q, T_k)
 
+    # sqrt(C/h)で割る
     # Scale
     outputs = outputs / (K_.get_shape().as_list()[-1] ** 0.5)
 
     # Key Masking
+    # keysは可変の長さを持つ(keys_lengthで定義)、値のある要素だけを示すマスクを生成する
     key_masks = tf.sequence_mask(keys_length, tf.shape(keys)[1])   # (N, T_k)
+    # ヘッドの数だけ複製する
     key_masks = tf.tile(key_masks, [num_heads, 1])  # (h*N, T_k)
+    # クエリの数だけマスクを複製
     key_masks = tf.tile(tf.expand_dims(key_masks, 1), [1, tf.shape(queries)[1], 1])  # (h*N, T_q, T_k)
 
+    # -2^32+1は無限大として考える、outputsと同じ形を持つすべての要素が無限大の行列paddingsを作る
     paddings = tf.ones_like(outputs) * (-2 ** 32 + 1)
+    # outputsにマスクを適用する、key_masksがTrueの要素はoutputsで、Falseはpaddingsで塗りつぶす
     outputs = tf.where(key_masks, outputs, paddings)  # (h*N, T_q, T_k)
 
+    # 文章のトランスフォーマーとは違い、予測に必要な以外の将来の行動をマスクする必要はないので実装していない
     # Causality = Future blinding: No use, removed
 
+    # 最後の次元、T_kのaxisでsoftmaxが実行される
     # Activation
     outputs = tf.nn.softmax(outputs)  # (h*N, T_q, T_k)
 
+    # keysのマスキングは行ったが、queryのマスキングは行っていないので行う
     # Query Masking
     query_masks = tf.sequence_mask(queries_length, tf.shape(queries)[1], dtype=tf.float32)   # (N, T_q)
     query_masks = tf.tile(query_masks, [num_heads, 1])  # (h*N, T_q)
@@ -425,7 +462,6 @@ def feedforward(inputs,
   Returns:
     A 3d tensor with the same shape and dtype as inputs
   '''
-  """トランスフォーマーのFeed Forwardと残差接続"""
   with tf.variable_scope(scope, reuse=reuse):
     # Inner layer
     params = {"inputs": inputs, "filters": num_units[0], "kernel_size": 1,
@@ -443,7 +479,7 @@ def feedforward(inputs,
     # Normalize
     outputs = normalize(outputs)
 
-  return outputs
+  return outputs  # [N, T, C]
 
 def normalize(inputs,
         epsilon=1e-8,
@@ -462,23 +498,28 @@ def normalize(inputs,
   Returns:
     A tensor with the same shape and data dtype as `inputs`.
   '''
-  """レイヤー正規化を行う"""
   with tf.variable_scope(scope, reuse=reuse):
+    # 入力の形
     inputs_shape = inputs.get_shape()
     params_shape = inputs_shape[-1:]
 
+    # 入力の平均と分散
     mean, variance = tf.nn.moments(inputs, [-1], keep_dims=True)
+    # バイアス
     beta = tf.Variable(tf.zeros(params_shape))
+    # 重み
     gamma = tf.Variable(tf.ones(params_shape))
+    # 正規化、eplisonは0除算を防ぐためのパラメーター
+    # TODO gamma、betaはブロードキャストされる？
     normalized = (inputs - mean) / ((variance + epsilon) ** (.5))
     outputs = gamma * normalized + beta
 
   return outputs
 
-
-def extract_axis_1(data, ind):
-  batch_range = tf.range(tf.shape(data)[0])
-  indices = tf.stack([batch_range, ind], axis=1)
-  res = tf.gather_nd(data, indices)
-  return res
+# この関数は使われていない
+# def extract_axis_1(data, ind):
+#   batch_range = tf.range(tf.shape(data)[0])
+#   indices = tf.stack([batch_range, ind], axis=1)
+#   res = tf.gather_nd(data, indices)
+#   return res
 
